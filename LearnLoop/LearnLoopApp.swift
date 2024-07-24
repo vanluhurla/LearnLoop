8ca557ed4235ec35acf0feb40d8f61e60b262da1
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2024-07-24 18:29:48 +0100

    Add files to repository on GitHub.

---
+++ LearnLoop/LearnLoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnLoopApp.swift
+//  LearnLoop
+//
+//  Created by Vanessa Hurla on 24/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct LearnLoopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
