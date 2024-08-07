7ff7497e9be29791d3426f09f4d72565abea08db
vanluhurla <94018863+vanluhurla@users.noreply.github.com>
2024-08-07 15:23:19 +0100

    Remove Edit button and adjust the home screen.

---
+++ LearnLoop/View/CardView/LLCardView.swift
@@ -0,0 +1,18 @@
+//
+//  LLCardView.swift
+//  LearnLoop
+//
+//  Created by Vanessa Hurla on 07/08/2024.
+//
+
+import SwiftUI
+
+struct LLCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LLCardView()
+}
